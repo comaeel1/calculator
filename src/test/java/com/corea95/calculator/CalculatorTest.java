87e437bb3df47204ebb2ed1fd9405c2a0bad0eef
Yeon Beom Jeong <dusqja231@naver.com>
2020-05-15 22:20:17 +0900

    Add sum application

---
+++ src/test/java/com/corea95/calculator/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.corea95.calculator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void sum() {
+        assertEquals(5, calculator.sum(2, 3));
+        }
+}
