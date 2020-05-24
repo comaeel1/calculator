ee582a65e6c4a09bdd42735a24498f46ed97d701
Yeon Beom Jeong <dusqja231@naver.com>
2020-05-24 16:02:20 +0900

    add jacoco test

--- src/main/java/com/corea95/calculator/Calculator.java
+++ src/main/java/com/corea95/calculator/Calculator.java
@@ -5,4 +5,7 @@ public class Calculator {
         int sum(int a, int b) {
             return a + b;
             }
+        int sub(int a, int b) {
+                return a - b;
+                }
 }
